d00b558155c617ce1ca016cb869a8f81ea585265
Gesem-Martinez <109618325+Gesem-Martinez@users.noreply.github.com>
2024-04-20 19:55:51 -0600

    Add PokeAPI integration to fetch location areas in 20 item batches

--- commands.go
+++ commands.go
@@ -1,27 +1,118 @@
 package main
 
 import (
-  "fmt"
-  "os"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+  "encoding/json"
+  "errors"
 )
 
-func commandHelp() error {
+type LocationAreas struct {
+  Count int `json:"count"`
+  Next *string `json:"next"`
+  Previous *string `json:"previous"`
+  Results []struct {
+    Name string `json:"name"`
+    URL string `json:"url"`
+  } `json:"results"`
+}
+
+func commandHelp(config *Config) error {
   fmt.Println("\nWelcome to the Gokedex!")
   fmt.Println("Available commands and usage:\n\n")
 
   commandMap := getCommands()
 
   for _, val := range commandMap {
-    fmt.Printf("%s: %s", val.name, val.description)
-    fmt.Println()
+    fmt.Printf("%s: %s\n", val.name, val.description)
   }
 
-  fmt.Println("\n")
+  fmt.Println()
 
   return nil
 }
 
-func commandExit() error {
+func commandExit(config *Config) error {
   os.Exit(0)
   return nil
+}
+
+// Fetch and Parse locations from PokeAPI
+func getLocations(URL string) (final LocationAreas, err error) {
+  var result LocationAreas
+  var baseRequestURL string = "https://pokeapi.co/api/v2/location-area/?limit=20"
+
+  if URL == "" {
+    URL = baseRequestURL
+  }
+
+  res, err := http.Get(URL)
+  if err != nil {
+    fmt.Printf("Error fetching locations: %s\n", err)
+  }
+
+  body, err := io.ReadAll(res.Body)
+  res.Body.Close()
+
+  if res.StatusCode > 299 {
+    fmt.Printf("Response failed with status code: %d and\nbody: %s\n", res.StatusCode, body)
+    return LocationAreas{}, nil
+  }
+  if err != nil {
+    fmt.Printf("Error during response: %s\n", err)
+    return LocationAreas{}, nil
+  }
+
+  json.Unmarshal(body, &result)
+
+  return result, nil
+}
+
+func baseMap(locationsData LocationAreas, config *Config) error {
+  if locationsData.Next != nil {
+    config.Next = *locationsData.Next
+  } else {
+    config.Next = "nil"
+  }
+  
+  if locationsData.Previous != nil {
+    config.Previous = *locationsData.Previous
+  } else {
+    config.Previous = ""
+  }
+
+  fmt.Println("Locations Fetched:")
+  fmt.Println(locationsData.Results)
+  return nil
+}
+
+func commandMap(config *Config) error {
+
+  if config.Next == "nil" {
+    return errors.New("There are no more locations.")
+  }
+
+  locationsData, err := getLocations(config.Next)
+
+  if err != nil {
+    return err
+  }
+
+  return baseMap(locationsData, config)
+}
+
+func commandMapb(config *Config) error {
+  if config.Previous == ""{
+    return errors.New("There are no previous locations.")
+  }
+
+  locationsData, err := getLocations(config.Previous)
+
+  if err != nil {
+    return err
+  }
+
+  return baseMap(locationsData, config)
 }
--- repl.go
+++ repl.go
@@ -7,13 +7,23 @@ import (
   "strings"
 )
 
+type Config struct {
+  Next string
+  Previous string
+}
+
 type cliCommand struct {
   name string
   description string
-  callback func() error
+  callback func(config *Config) error
 }
 
 func startRepl(){
+  config := Config{}
+
+  config.Next = ""
+  config.Previous = ""
+
   availableCommands := getCommands()
 
   var prompt string = "Gokedex > "
@@ -30,7 +40,10 @@ func startRepl(){
     }
 
     if command, ok := availableCommands[commandFields[0]]; ok {
-      command.callback()
+      err := command.callback(&config)
+      if err != nil {
+        fmt.Println(err)
+      }
     }
 
   }
@@ -47,6 +60,16 @@ func getCommands() map[string]cliCommand {
       name: "exit",
       description: "Exit the program",
       callback: commandExit,
+    },
+    "map": {
+      name: "map",
+      description: "Display the names of the next 20 location areas in the Pokemon world.",
+      callback: commandMap,
+    },
+    "mapb": {
+      name: "mapb",
+      description: "Display the names of the previous 20 location areas in the Pokemon world.",
+      callback: commandMapb,
     },
   }
 }
